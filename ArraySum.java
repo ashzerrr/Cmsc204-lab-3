7e4e2d288e07f4d0ce7abab1b9d2ea06a1377bc6
ashzerrr <129709857+ashzerrr@users.noreply.github.com>
2024-02-19 20:31:54 -0500

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,30 @@
+package HW;
+	
+	
+	public class ArraySum {
+		
+		public int sumOfArray(Integer[] a, int index) {
+			
+			if(index == 0)
+				
+				return a[index];
+			
+			else
+				
+				return a[index] + sumOfArray(a, --index);
+			
+		}
+
+	}
+	
+	
+	
+	
+	
+	
+	
+
+
+	
+
+
